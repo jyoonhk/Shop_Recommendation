25feecf9a5c476a890e1243770ccd01448af25d5
edwardasross <edwardasross@gmail.com>
2021-05-10 14:08:10 +0800

    Added CV2 detection with Yolo

--- 4. Deployment/testwithpages/model.py
+++ 4. Deployment/testwithpages/model.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#dataframe
+userdb = pd.read_csv('csv/User_db.csv')
 
 
 def app():
@@ -10,4 +15,12 @@ def app():
     with st.beta_container():
         st.write("This is inside the container")
         st.bar_chart(np.random.randn(50, 3))
-    st.write("This is outside the container")
+
+        st.write("Database of scraped images cataloged")
+        st.dataframe(userdb)
+
+        category_counts = userdb['Category'].value_counts()
+        st.bar_chart(category_counts)
+
+        gen_cat = pd.crosstab(userdb['Category'],userdb['Gender']).plot(kind="bar",stacked=True)
+        st.pyplot(gen_cat)
